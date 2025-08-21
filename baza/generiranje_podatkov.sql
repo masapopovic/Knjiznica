6ff48e281f81bef98c95fb7586d77a691d6584ec
TinkaNapret <tn29170@student.uni-lj.si>
2025-08-21 17:05:59 +0200

    dodajanje podatkov za bralno srečanje

---
+++ baza/generiranje_podatkov.sql
@@ -0,0 +1,11 @@
+INSERT INTO bralno_srecanje (prostor, datum, naziv_in_opis, id_knjige) VALUES
+('Čitalnica', '2025-09-10 17:00:00', 'Srečanje z avtorjem romana Izkrivljene igre', 1),
+('Oddelek za otroke', '2025-09-12 16:00:00', 'Branje in delavnica za otroke', 2),
+('Soba za dogodke', '2025-09-15 18:00:00', 'Pogovor o literaturi za odrasle', 3),
+('Čitalnica', '2025-09-18 17:30:00', 'Literarni večer: poezija in proza', 4),
+('Oddelek za otroke', '2025-09-20 16:00:00', 'Branje pravljic in ustvarjalna delavnica', 5),
+('Soba za dogodke', '2025-09-22 18:30:00', 'Srečanje za ljubitelje kriminalk', 6),
+('Čitalnica', '2025-09-25 17:00:00', 'Diskusija o romanih 21. stoletja', 7),
+('Oddelek za otroke', '2025-09-28 16:30:00', 'Otroški literarni klub', 8),
+('Soba za dogodke', '2025-10-01 18:00:00', 'Branje poezije in interpretacija', 9),
+('Čitalnica', '2025-10-05 17:00:00', 'Srečanje z mladinsko literaturo', 10);
