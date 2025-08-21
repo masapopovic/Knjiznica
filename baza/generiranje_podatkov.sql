683645a0e6df03db1053d128374e2d6b61ddb09d
TinkaNapret <tn29170@student.uni-lj.si>
2025-08-21 17:10:39 +0200

    dodajanje podatkov v bazo

--- baza/generiranje_podatkov.sql
+++ baza/generiranje_podatkov.sql
@@ -9,3 +9,19 @@ INSERT INTO bralno_srecanje (prostor, datum, naziv_in_opis, id_knjige) VALUES
 ('Oddelek za otroke', '2025-09-28 16:30:00', 'Otroški literarni klub', 8),
 ('Soba za dogodke', '2025-10-01 18:00:00', 'Branje poezije in interpretacija', 9),
 ('Čitalnica', '2025-10-05 17:00:00', 'Srečanje z mladinsko literaturo', 10);
+
+
+INSERT INTO clan (ime, priimek, uporabnisko_ime, geslo, email, status_clana) VALUES
+('Ana', 'Novak', 'ana.novak', 'hash1', 'ana.novak@email.com', 'aktiven'),
+('Marko', 'Kovač', 'marko.kovac', 'tajno456', 'marko.kovac@email.com', 'aktiven'),
+('Lina', 'Zupan', 'lina.zupan', 'mojegeslo789', 'lina.zupan@email.com', 'neaktiven'),
+('Peter', 'Horvat', 'peter.horvat', '123geslo', 'peter.horvat@email.com', 'aktiven'),
+('Maja', 'Vidmar', 'maja.vidmar', 'geslo321', 'maja.vidmar@email.com', 'neaktiven');
+
+
+INSERT INTO ocena (ocena, datum, komentar, id_clana, id_knjige) VALUES
+(5, '2025-08-21', 'Odlična knjiga, zelo priporočam!', 1, 1),
+(4, '2025-08-20', 'Zanimiva zgodba, nekaj delov bi lahko bili krajši.', 2, 5),
+(3, '2025-08-19', 'Povprečna knjiga, nič posebnega.', 4, 3),
+(2, '2025-08-18', 'Nisem bil navdušen, stil pisanja ni bil všeč.', 4,1),
+(5, '2025-08-17', 'Neverjetna knjiga, prebrala bi še več od tega avtorja!', 3, 5);
