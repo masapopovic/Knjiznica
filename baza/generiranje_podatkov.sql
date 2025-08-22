f46a80d72265508c094e205a6fb0ac2d43f8854e
TinkaNapret <tn29170@student.uni-lj.si>
2025-08-22 10:01:35 +0200

    popravek pri podatkih

--- baza/generiranje_podatkov.sql
+++ baza/generiranje_podatkov.sql
@@ -1,22 +1,22 @@
 INSERT INTO bralno_srecanje (prostor, datum, naziv_in_opis, id_knjige) VALUES
-('Čitalnica', '2025-09-10 17:00:00', 'Srečanje z avtorjem romana Izkrivljene igre', 1),
-('Oddelek za otroke', '2025-09-12 16:00:00', 'Branje in delavnica za otroke', 2),
+('Čitalnica', '2025-09-10 17:00:00', 'Srečanje z avtorjem knjige', 13),
+('Oddelek za otroke', '2025-09-12 16:00:00', 'Otroški literarni klub', 10),
 ('Soba za dogodke', '2025-09-15 18:00:00', 'Pogovor o literaturi za odrasle', 3),
-('Čitalnica', '2025-09-18 17:30:00', 'Literarni večer: poezija in proza', 4),
-('Oddelek za otroke', '2025-09-20 16:00:00', 'Branje pravljic in ustvarjalna delavnica', 5),
-('Soba za dogodke', '2025-09-22 18:30:00', 'Srečanje za ljubitelje kriminalk', 6),
-('Čitalnica', '2025-09-25 17:00:00', 'Diskusija o romanih 21. stoletja', 7),
-('Oddelek za otroke', '2025-09-28 16:30:00', 'Otroški literarni klub', 8),
-('Soba za dogodke', '2025-10-01 18:00:00', 'Branje poezije in interpretacija', 9),
-('Čitalnica', '2025-10-05 17:00:00', 'Srečanje z mladinsko literaturo', 10);
+('Čitalnica', '2025-09-18 17:30:00', 'Srečanje z avtorjem knjige', 29),
+('Oddelek za otroke', '2025-09-20 16:00:00', 'Otroški literarni klub', 79),
+('Soba za dogodke', '2025-09-22 18:30:00', 'Branje poezije in interpretacija', 18),
+('Čitalnica', '2025-09-25 17:00:00', 'Pogovor o literaturi za odrasle', 5),
+('Oddelek za otroke', '2025-09-28 16:30:00', 'Otroški literarni klub', 16),
+('Soba za dogodke', '2025-10-01 18:00:00', 'Branje poezije in interpretacija', 55),
+('Čitalnica', '2025-10-05 17:00:00', 'Srečanje z avtorjem knjige', 68);
 
 
 INSERT INTO clan (ime, priimek, uporabnisko_ime, geslo, email, status_clana) VALUES
 ('Ana', 'Novak', 'ana.novak', 'hash1', 'ana.novak@email.com', 'aktiven'),
-('Marko', 'Kovač', 'marko.kovac', 'tajno456', 'marko.kovac@email.com', 'aktiven'),
-('Lina', 'Zupan', 'lina.zupan', 'mojegeslo789', 'lina.zupan@email.com', 'neaktiven'),
-('Peter', 'Horvat', 'peter.horvat', '123geslo', 'peter.horvat@email.com', 'aktiven'),
-('Maja', 'Vidmar', 'maja.vidmar', 'geslo321', 'maja.vidmar@email.com', 'neaktiven');
+('Marko', 'Kovač', 'marko.kovac', 'hash2', 'marko.kovac@email.com', 'aktiven'),
+('Lina', 'Zupan', 'lina.zupan', 'hash3', 'lina.zupan@email.com', 'neaktiven'),
+('Peter', 'Horvat', 'peter.horvat', 'hash4', 'peter.horvat@email.com', 'aktiven'),
+('Maja', 'Vidmar', 'maja.vidmar', 'hash5', 'maja.vidmar@email.com', 'neaktiven');
 
 
 INSERT INTO ocena (ocena, datum, komentar, id_clana, id_knjige) VALUES
