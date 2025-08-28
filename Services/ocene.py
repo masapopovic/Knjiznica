2bf144e640b5c61c2569e3d91e558b1ae39ea4dc
masapopovic <mp13284@student.uni-lj.si>
2025-08-28 11:56:08 +0200

    ocene service

--- Services/ocene.py
+++ Services/ocene.py
@@ -28,29 +28,23 @@ class OcenaService:
         return self.repo.dobi_ocene_po_naslovu_in_avtorju(naslov_knjige, ime_avtorja, priimek_avtorja)
 
     
-    def dodaj_oceno_za_prijavljenega(self, uporabnisko_ime: str, id_knjige: int, ocena: int, komentar: str = ""):
-            """
-            Doda oceno knjige za člana, ki je prijavljen z uporabniškim imenom.
-            """
-            # Preveri, ali je ocena veljavna
-            if not (1 <= ocena <= 5):
-                raise ValueError("Ocena mora biti med 1 in 5.")
 
-            # Dobi id_clana preko uporabniškega imena
-            clan = self.repo.dobi_clana_po_uporabniskem_imenu(uporabnisko_ime)
-            if not clan:
-                raise ValueError("Član ne obstaja.")
+    def dodaj_oceno(self, id_clana: int, id_knjige: int, ocena: int, komentar: str = ""):
+        """
+        Doda oceno knjige za člana.
+        """
+        if not (1 <= ocena <= 5):
+            raise ValueError("Ocena mora biti med 1 in 5.")
 
-            # Ustvari objekt Ocena
-            nova_ocena = Ocena(
-                ocena=ocena,
-                datum=None,  # repo lahko nastavi trenutni datum
-                komentar=komentar,
-                id_clana=clan.id_clana,
-                id_knjige=id_knjige
-            )
+        nova_ocena = Ocena(
+            ocena=ocena,
+            datum=None,  # repo lahko nastavi trenutni datum
+            komentar=komentar,
+            id_clana=id_clana,
+            id_knjige=id_knjige
+        )
 
-            # Pošlji v repo
-            self.repo.dodaj_oceno(nova_ocena)
+        self.repo.dodaj_oceno(nova_ocena)
+
 
             
